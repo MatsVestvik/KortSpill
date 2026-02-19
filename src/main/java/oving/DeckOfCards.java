13e9ad97e2b2abb198550af50f54d6a61c2013ad
MatsVestvik <mats.o.vestvik@gmail.com>
2026-02-19 08:52:54 +0100

    project compiles

---
+++ src/main/java/oving/DeckOfCards.java
@@ -0,0 +1,13 @@
+package oving;
+
+import java.util.ArrayList;
+
+public class DeckOfCards {
+
+  private ArrayList<PlayingCard> deck;
+
+  public DeckOfCards() {
+    deck = new ArrayList<>();
+
+  }
+}
