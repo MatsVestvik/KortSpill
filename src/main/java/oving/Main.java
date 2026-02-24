0c64a177551a981224d6f8fab172adf9861949f4
MatsVestvik <mats.o.vestvik@gmail.com>
2026-02-24 10:16:00 +0100

    working SceneManager

--- src/main/java/oving/Main.java
+++ src/main/java/oving/Main.java
@@ -10,8 +10,6 @@ public class Main extends Application {
   @Override
   public void start(Stage stage) {
 
-    Card card = new Card(11, 'C');
-    card.createDisplayObject(5);
     ScreenManager screenManager = new ScreenManager(stage);
     stage.show();
     StartScreen startScreen = new StartScreen(screenManager);
